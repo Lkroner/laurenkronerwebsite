078be56a87ab33651e055b9daa4bf9c7e7a97d77
Lauren Kroner <lauren.kroner@gmail.com>
2015-08-22 23:30:31 -0700

    Cleaning up unused code.

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 
 var nodemailer = require('nodemailer');
-// var smtpTransport = require('nodemailer-smtp-transport');
 var sgTransport = require('nodemailer-sendgrid-transport');
 
 var path = require("path");
@@ -12,10 +11,6 @@ var url = require('url');
 
 // GET / - Home page.
 router.get('/', function(request, res, next) {
-  var url_parts = url.parse(request.url, true);
-  var query = url_parts.query;
-  // console.log('GET /');
-  console.log(query);
   res.sendFile(path.join(__dirname, "../views/index.html"));
 });
 
