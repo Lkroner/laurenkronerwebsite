784ede555738bbb24b125165d87db09cbacd160b
Lauren Kroner <lauren.kroner@gmail.com>
2015-08-22 19:04:31 -0700

    Will need that.

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,7 @@ router.get('/send', function(req, res, next) {
 	var transporter = nodemailer.createTransport(smtpTransport({
 		service: process.env.NODEMAILER_SERVICE,
 		auth: {
-			user: process.env.NODEMAILER_USER
+			user: process.env.NODEMAILER_USER,
 			pass: process.env.NODEMAILER_PASS
 		}
 	}));
