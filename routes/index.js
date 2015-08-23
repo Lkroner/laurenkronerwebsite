2f3f1c864e1d3cdd03de9ba2e4ce0127da578465
Lauren Kroner <lauren.kroner@gmail.com>
2015-08-22 17:50:25 -0700

    Move smtp server creation in scope with environment variables and update to version nodemailer 1.4.0 with new smtp transporter methods.

--- routes/index.js
+++ routes/index.js
@@ -2,29 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 var nodemailer = require('nodemailer');
+var smtpTransport = require('nodemailer-smtp-transport');
+
 var path = require("path");
 var url = require('url');
 
+// ------------------Routing Started ------------------------
 
-/*
-Here we are configuring our SMTP Server details.
-STMP is mail server which is responsible for sending and recieving email.
-*/
-
-var smtpTransport = nodemailer.createTransport("SMTP", {
-	service: process.env.NODEMAILER_SERVICE,
-	auth: {
-		user: process.env.NODEMAILER_USER,
-		pass: process.env.NODEMAILER_PASS
-	}
-});
-
-/*------------------SMTP Over-----------------------------*/
-
-
-/*------------------Routing Started ------------------------*/
-
-/* GET home page. */
+// GET / - Home page.
 router.get('/', function(request, res, next) {
   var url_parts = url.parse(request.url, true);
   var query = url_parts.query;
@@ -34,22 +19,36 @@ router.get('/', function(request, res, next) {
 });
 
 
-/* GET send page. */
+// GET /send - Will send email via nodemailer SMTP transporter.
 router.get('/send', function(req, res, next) {
+	
+	// Here we are configuring our SMTP Server details.
+	// STMP is mail server which is responsible for sending and recieving email.
+	var transporter = nodemailer.createTransport(smtpTransport({
+		service: process.env.NODEMAILER_SERVICE,
+		auth: {
+			user: process.env.NODEMAILER_USER,
+			pass: process.env.NODEMAILER_PASS
+		}
+	}));
+
+	// ------------------SMTP Over-----------------------------
 
 	var mailOptions = {
 		to: "lauren.kroner@gmail.com",
-		subject: "Lauren, " + req.query.first_name + ", " + req.query.email + "sent you a message from laurenkroner.com",
-		text: req.query.message
+		subject: "You got a message from " + req.query.email + "!",
+		text:
+			// "Hey Lauren," +
+			// req.query.
+			// req.query.message
 	};
+
 	console.log("Here's mailOptions", mailOptions);
-	console.log("process.env.NODEMAILER_USER: ", process.env.NODEMAILER_USER);
-	console.log("process.env.NODEMAILER_PASS: ", process.env.NODEMAILER_PASS);
-	smtpTransport.sendMail(mailOptions, function(error, response){
+	transporter.sendMail(mailOptions, function(error, response){
 		if (error) {
 			console.log(error);
 		} else {
-			console.log("Message sent: " + response.message);
+			console.log("Message sent: " + JSON.stringify(response));
 			res.end("sent");
 		}
 	});
